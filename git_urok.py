42df441f8b0a516d2a39ffcb20e47d8e44cb29e9
Armen281989 <61555835+Armen281989@users.noreply.github.com>
2020-03-03 01:23:15 +0400

    zuyg tiv

--- git_urok.py
+++ git_urok.py
@@ -1,56 +1,64 @@
-import time
-print(8)
-plis = input('plis write -  ') == '-'
-print(3)
-time.sleep(2)
-print('8-3=5',plis, '\n thanks',)
+print('greq 0-10 zuyg tver ')
+zuyg = int(input('0-10zuyg tiv ')) 
 
-print(8)
-plis1 = input('plis write +  ') == '+'
-print(3)
-time.sleep(2)
-print('8+3=11',plis1,'\n very good')
-
-print(8)
-plis2 = input('plis write *  ') == '*'
-print(3)
-time.sleep(2)
-print('8*3=24',plis2,'\n thanks')
-
-print(8)
-plis3 = input('plis write /  ') == '/'
-print(3)
-time.sleep(2)
-print('8/3=2,66666667',plis3,'\n very good')
+print(zuyg == 2 or zuyg == 4 or zuyg == 6 or zuyg == 8 or zuyg ==10)
 
 
 
-def add(x, y):
-   return x + y
 
-def subtract(x, y):
-   return x - y
+# import time
+# print(8)
+# plis = input('plis write -  ') == '-'
+# print(3)
+# time.sleep(2)
+# print('8-3=5',plis, '\n thanks',)
 
-def multiply(x, y):
-   return x * y
+# print(8)
+# plis1 = input('plis write +  ') == '+'
+# print(3)
+# time.sleep(2)
+# print('8+3=11',plis1,'\n very good')
 
-def divide(x, y):
-   return x / y
+# print(8)
+# plis2 = input('plis write *  ') == '*'
+# print(3)
+# time.sleep(2)
+# print('8*3=24',plis2,'\n thanks')
+
+# print(8)
+# plis3 = input('plis write /  ') == '/'
+# print(3)
+# time.sleep(2)
+# print('8/3=2,66666667',plis3,'\n very good')
+
+
+
+# def add(x, y):
+#    return x + y
+
+# def subtract(x, y):
+#    return x - y
+
+# def multiply(x, y):
+#    return x * y
+
+# def divide(x, y):
+#    return x / y
  
-num1 = float(input("Enter first number: "))
-choice = input("Enter choice(+/-/*//): ")
-num2 = float(input("Enter second number: "))
+# num1 = float(input("Enter first number: "))
+# choice = input("Enter choice(+/-/*//): ")
+# num2 = float(input("Enter second number: "))
 
-if choice == '+':
-   print(num1,"+",num2,"=", add(num1,num2))
+# if choice == '+':
+#    print(num1,"+",num2,"=", add(num1,num2))
 
-elif choice == '-':
-   print(num1,"-",num2,"=", subtract(num1,num2))
+# elif choice == '-':
+#    print(num1,"-",num2,"=", subtract(num1,num2))
 
-elif choice == '*':
-   print(num1,"*",num2,"=", multiply(num1,num2))
+# elif choice == '*':
+#    print(num1,"*",num2,"=", multiply(num1,num2))
 
-elif choice == '/':
-   print(num1,"/",num2,"=", divide(num1,num2))
-else:
-   print("Invalid input:")
+# elif choice == '/':
+#    print(num1,"/",num2,"=", divide(num1,num2))
+# else:
+#    print("Invalid input:")
