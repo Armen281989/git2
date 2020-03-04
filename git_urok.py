a4c6b4875cce1eb0d394bac991568379ee64f19b
Armen281989 <61555835+Armen281989@users.noreply.github.com>
2020-03-05 00:42:15 +0400

    homwork 7

--- git_urok.py
+++ git_urok.py
@@ -33,32 +33,32 @@ print(zuyg == 2 or zuyg == 4 or zuyg == 6 or zuyg == 8 or zuyg ==10)
 
 
 
-# def add(x, y):
-#    return x + y
+def add(x, y):
+   return x + y
 
-# def subtract(x, y):
-#    return x - y
+def subtract(x, y):
+   return x - y
 
-# def multiply(x, y):
-#    return x * y
+def multiply(x, y):
+   return x * y
 
-# def divide(x, y):
-#    return x / y
+def divide(x, y):
+   return x / y
  
-# num1 = float(input("Enter first number: "))
-# choice = input("Enter choice(+/-/*//): ")
-# num2 = float(input("Enter second number: "))
+num1 = float(input("Enter first number: "))
+choice = input("Enter choice(+/-/*//): ")
+num2 = float(input("Enter second number: "))
 
-# if choice == '+':
-#    print(num1,"+",num2,"=", add(num1,num2))
+if choice == '+':
+   print(num1,"+",num2,"=", add(num1,num2))
 
-# elif choice == '-':
-#    print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '-':
+   print(num1,"-",num2,"=", subtract(num1,num2))
 
-# elif choice == '*':
-#    print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '*':
+   print(num1,"*",num2,"=", multiply(num1,num2))
 
-# elif choice == '/':
-#    print(num1,"/",num2,"=", divide(num1,num2))
-# else:
-#    print("Invalid input:")
+elif choice == '/':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+   print("Invalid input:")
---
+++ homwork7.py
@@ -0,0 +1,28 @@
+# arshak = input('greq cer anun@ ')
+# tariq = int(input('greq cer tariq@ '))
+# if arshak == 'arshak' and tariq == 25:
+# 	print('menq cez enq pntrum')
+# else:
+# 	print('duq cheq hamapatasxanum mer pahangnerin') 
+
+# import random
+# anun = input('inch e cer anun@? ')
+# user_rand = int(input('greq 1-10 tiv '))
+# comp_rand = random.randint(1,10)
+# print(comp_rand)
+# if comp_rand == user_rand:
+# 	print(user_rand, '=', comp_rand,'havasar, voch voq chhaxtec')
+# elif comp_rand > user_rand:
+# 	print(anun, comp_rand,'>', user_rand, " duq partveciq")
+# else:
+# 	print(anun, comp_rand, '<', user_rand, ' duq haxteciq')
+
+
+print('mut gorcelu hamar greq cer login ev parol ')
+anun = input('inch e cer anun@? ')
+login = input('cer login ')
+pasword = input('cer gaxtnabar ')
+if login == 'arshak' and pasword == 'pas':
+	print('barev',anun, 'duq mutq gorceciq cer profil')
+else:
+	print(anun,'sxal login kam gaxtnabar')
